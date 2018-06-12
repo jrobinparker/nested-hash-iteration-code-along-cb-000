d22469127ebc0f4c9b88682f2cf74dcb35d07d60
Jeremy Parker <jrobinparker@gmail.com>
2018-06-12 22:23:34 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -28,7 +28,7 @@ def remove_strawberry(contacts)
      }
    }
    
-   contacts.each do |person, data|
+   contacts.each do |person, contact_details_hash|
      contact_details_hash.each do |attribute, data|
        binding.pry
      end
